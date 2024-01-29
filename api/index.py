3c000cd4e78d6f3bb075dfe144fbd5b5e76135f8
Leonard Pauli <leonardpauli@me.com>
2024-01-29 21:34:12 +0100

    trigger deploy test

--- api/index.py
+++ api/index.py
@@ -3,8 +3,17 @@ from http.server import BaseHTTPRequestHandler
 class handler(BaseHTTPRequestHandler):
 
 	def do_GET(self):
+
+		# respond with json {"message": "Hello World!"}
+		self.send_response(200)
+		self.send_header('Content-type','application/json')
+		self.end_headers()
+		self.wfile.write('{"message": "Hello World!"}'.encode('utf-8'))
+
+		"""
 		self.send_response(200)
 		self.send_header('Content-type','text/plain')
 		self.end_headers()
 		self.wfile.write('Hello, world!'.encode('utf-8'))
+		"""
 		return
